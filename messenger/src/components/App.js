fec3362ae53eb7a19e1b8c1ab9350d87d39dd05c
Daniil_Astakhov <daniil.astakhov_it@outlook.com>
2023-06-05 19:08:14 +0300

    server

---
+++ messenger/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppRoutes from "./AppRoutes";
+
+
+const App = () => {
+    return (
+        <div className="container">
+            <AppRoutes />
+        </div>
+    )
+};
+
+export default App;
